35588999d44bf5069195ffb904b95909857f609c
Kirill Grishin <sunman5@mail.ru>
2013-03-17 15:54:30 +0900

    Now really adds RoutesFinder class

--- src/main/scala/Takashi/RoutesFinder.scala
+++ src/main/scala/Takashi/RoutesFinder.scala
@@ -1,12 +1,90 @@
 package Takashi
 
 /**
- * Created with IntelliJ IDEA.
- * User: Kirill
- * Date: 17.03.13
- * Time: 13:00
- * To change this template use File | Settings | File Templates.
+ *
  */
-class RoutesFinder {
+object RoutesFinder {
+
+  type Distance = Int
+  type Fare = Int
+
+  case class Station(name: String) {
+    def availableRoutes(implicit routes: Map[Station,List[(Station,Distance,Fare)]]) = routes(this)
+  }
+
+  case class Path(start: Station, goal: Station, visitedStations: List[Station], distance: Distance, fare: Fare) {
+    lazy val currentStation = visitedStations.headOption.getOrElse(start)
+    lazy val goalReached = visitedStations.headOption == Some(goal)
+    lazy val result = (visitedStations.reverse, distance, fare)
+  }
+
+}
+
+class RoutesFinder(input: List[(String, String, Int, Int)]) {
+
+  import RoutesFinder._
+
+  implicit val routes: Map[Station,List[(Station,Distance,Fare)]] = {
+    val initial: Map[Station,List[(Station, Distance, Fare)]] = Map().withDefaultValue(List())
+    (input foldLeft initial) {
+      case (acc, (start, goal, distance, fare)) =>
+        acc.updated(Station(start), (Station(goal), distance, fare)::acc(Station(start)))
+    }
+  }
+
+  private def solve(policy: Ordering[Path], pathsWithoutGoal: List[Path], pathsWithGoal: List[Path] = List()): List[Path] = {
+
+    val (newPathsWithGoal, newPathsWithoutGoal) = pathsWithoutGoal partition ( _.goalReached )
+
+    val mergedPathsWithGoal = newPathsWithGoal ++ pathsWithGoal
+    val bestPathsWithGoal =
+      if (mergedPathsWithGoal.isEmpty)
+        List()
+      else {
+        val bestPathWithGoal = mergedPathsWithGoal.minBy(path => path)(policy)
+        mergedPathsWithGoal filter ( policy.equiv(_, bestPathWithGoal) )
+      }
+
+    lazy val pathsWithoutGoalWithPossiblyBetterResults =
+      if (bestPathsWithGoal.isEmpty)
+        newPathsWithoutGoal
+      else
+        newPathsWithoutGoal filter ( policy.lteq(_, bestPathsWithGoal.head) )
+
+    if (pathsWithoutGoalWithPossiblyBetterResults.isEmpty)
+      bestPathsWithGoal
+    else {
+      // Extend activePathsWithPossiblyBetterResults with new stations (excluding visited ones)
+      val pathsWithNextStations = for {
+        path <- pathsWithoutGoalWithPossiblyBetterResults
+        res  <- path.currentStation.availableRoutes collect {
+          case (station, distance, fare) if ! (path.visitedStations contains station) =>
+            new Path(path.start, path.goal, station::path.visitedStations, path.distance + distance, path.fare + fare)
+        }
+      } yield res
+      // Make recursive call with updated input data
+      solve(policy, pathsWithNextStations, bestPathsWithGoal)
+    }
+
+  }
+
+  def findRoutes(start: String, goal: String, policy: Ordering[Path]) = {
+    val initialPath = Path(Station(start), Station(goal), List(), 0, 0)
+    solve(policy, List(initialPath))
+  }
+
+  def findRoutesByDistance(start: String, goal: String) = {
+    object ByDistance extends Ordering[Path] {
+      def compare(path1: Path, path2: Path) = path1.distance compare path2.distance
+    }
+    findRoutes(start, goal, ByDistance)
+  }
+
+  def findRoutesByFare(start: String, goal: String) = {
+    object ByFare extends Ordering[Path] {
+      def compare(path1: Path, path2: Path) = path1.fare compare path2.fare
+    }
+    findRoutes(start, goal, ByFare)
+  }
 
 }
--- src/test/scala/RoutesFinderSpec.scala
+++ src/test/scala/RoutesFinderSpec.scala
@@ -1,10 +1,42 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Kirill
- * Date: 17.03.13
- * Time: 14:41
- * To change this template use File | Settings | File Templates.
- */
-class RoutesFinderSpec {
+import org.specs2.mutable._
+
+class RoutesFinderSpec extends Specification {
+
+  import Takashi.RoutesFinder
+  import Takashi.RoutesFinder._
+
+  "Takashi.RoutesFinder" should {
+
+    val input = List(
+      ("A", "B", 5, 150),
+      ("A", "C", 10, 450),
+      ("B", "C", 3, 220),
+      ("B", "D", 15, 150),
+      ("C", "D", 4, 300),
+      ("Z", "X", 7, 220)
+    )
+
+    val routeFinder = new RoutesFinder(input)
+
+    "correctly parse input data into a map" in {
+
+      "where number of routes must be the same" in {
+        ((input groupBy { case (s, _, _, _) => s }).keys).map(Station(_)) === routeFinder.routes.keys
+      }
+
+    }
+
+    val reversedInput = input map { case (start, goal, d, f) => (goal, start, d, f) }
+    val reversedRoutesFinder = new RoutesFinder(reversedInput)
+
+    val normalByDistance = routeFinder.findRoutesByDistance("A","D")
+    val normalByFare = routeFinder.findRoutesByFare("A","D")
+
+    val reversedByDistance = reversedRoutesFinder.findRoutesByDistance("D","A")
+    val reversedByFare = reversedRoutesFinder.findRoutesByFare("D","A")
+
+
+  }
+
 
 }
